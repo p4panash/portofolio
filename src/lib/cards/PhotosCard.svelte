546b363fa07d7ffe8c516d86fbb4b57204d33e9a
p4panash <muntean.catalin.avram@gmail.com>
2025-10-06 22:34:08 +0300

    Use actual photos

--- src/lib/cards/PhotosCard.svelte
+++ src/lib/cards/PhotosCard.svelte
@@ -5,17 +5,18 @@
 	export let sizeStyling = '';
 
 	// Placeholder images - will be replaced with actual photos
-	const placeholderImages = [
-		'/images/photos/placeholder-1.jpg',
-		'/images/photos/placeholder-2.jpg',
-		'/images/photos/placeholder-3.jpg',
-		'/images/photos/placeholder-4.jpg',
-		'/images/photos/placeholder-5.jpg'
+	const images = [
+		'/images/photos/1.jpeg',
+		'/images/photos/2.jpeg',
+		'/images/photos/3.jpeg',
+		'/images/photos/4.jpeg',
+		'/images/photos/5.jpeg',
+		'/images/photos/6.jpg'
 	];
 </script>
 
 <Card {sizeStyling} noPadding>
 	<div class="relative w-full h-full rounded-lg overflow-hidden min-h-[200px]">
-		<Carousel images={placeholderImages} autoPlayInterval={5000} />
+		<Carousel {images} autoPlayInterval={5000} />
 	</div>
 </Card>
