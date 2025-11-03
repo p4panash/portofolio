4962e394510b6f96fcab98f8c062e8992547040a
p4panash <muntean.catalin.avram@gmail.com>
2025-11-03 12:20:08 +0200

    Fix eslint warning

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,6 @@ export default tseslint.config(
 		}
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/', '.vercel/']
+		ignores: ['build/', '.svelte-kit/', 'dist/', '.vercel/', '.github/badges/']
 	}
 );
